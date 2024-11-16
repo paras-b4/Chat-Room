fb03c423376b796e19450c382bc421b1167a8e58
unknown <py080398@gmail.com>
2024-11-16 17:13:14 +0530

    first commit

---
+++ src/main/java/com/paras/ChatRoom/controller/MessageController.java
@@ -0,0 +1,20 @@
+package com.paras.ChatRoom.controller;
+
+import com.paras.ChatRoom.models.Message;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MessageController {
+
+    @MessageMapping("/message")
+    @SendTo("/topic/return-to")
+    public Message getContent(@RequestBody Message message) throws InterruptedException {
+
+//        Thread.sleep(200);
+
+        return message;
+    }
+}
